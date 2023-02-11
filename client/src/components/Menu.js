eebb64529ccb417936a41d582e8273adfe409d07
J.Gross <jan.gross.cze@gmail.com>
2023-02-11 11:51:48 +0100

    Přidání základní struktury  context, globalContext, Reducer

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Menu = () => {
     return (
         <div>
-            <p>Tady bude menu: </p>
+            <Link to="/">Hlavní menu</Link>
+            <Link to="/add-material">Přidání materialu</Link>
+           
         </div>
     )
 }
